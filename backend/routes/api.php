87fef3f3a6593fe3f422a1117e3cc28c4218d9a3
tsukasaI <inoue.tsukasa.4@gmail.com>
2021-08-28 17:32:53 +0900

    recreate laravel project

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -17,10 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::get('/test', function () {
-    return response()->json([
-        'status' => 'ok',
-        'data' => 'success',
-    ]);
-});
